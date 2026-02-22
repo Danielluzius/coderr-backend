8540c826d0513ca52abe3e7c0daee8745a08da30
Daniel <Danielluzius@mail.de>
2026-02-22 15:14:57 +0100

    feat: implement admin interfaces for Offer, Order, UserProfile, and Review models with display and search configurations

--- offers_app/admin.py
+++ offers_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Offer, OfferDetail
+
+
+@admin.register(Offer)
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ['title', 'user', 'created_at', 'updated_at']
+    search_fields = ['title', 'description']
+
+
+@admin.register(OfferDetail)
+class OfferDetailAdmin(admin.ModelAdmin):
+    list_display = ['offer', 'offer_type', 'price', 'delivery_time_in_days']
+    list_filter = ['offer_type']
--- orders_app/admin.py
+++ orders_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer_user', 'business_user', 'title', 'status', 'created_at']
+    list_filter = ['status']
+    search_fields = ['title', 'customer_user__username', 'business_user__username']
